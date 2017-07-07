4125d25c348c920bba0930bb024b12843782bc3d
Axel <latiosaxe@gmail.com>
2017-07-07 01:35:29 -0500

    Start

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+HACKATON.main = (function(){
+
+    var _initVars = function _initVars(){
+        console.info("PEEEEEEEERRO!!");
+    };
+
+    return {
+        init: function init() {
+            _initVars();
+        }
+    }
+})();
